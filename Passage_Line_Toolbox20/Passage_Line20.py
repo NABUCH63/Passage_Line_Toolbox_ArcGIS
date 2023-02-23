78c99696f94ae6fef0037663a3b766d2de80c43d
NABUCH63 <nodakbuch@hotmail.com>
2023-02-23 14:02:48 -0800

    Updated passage line tool, saved as 2.0. Now takes in input of polyline or polygon, and adjusts the output interactions according to user input.

--- Passage_Line_Toolbox20/Passage_Line20.py
+++ Passage_Line_Toolbox20/Passage_Line20.py
@@ -8,34 +8,85 @@ import arcpy
 ## Define parameter inputs, outputs
 TrackLines = str(arcpy.GetParameterAsText(0))
 PassageLines = str(arcpy.GetParameterAsText(1))
-NameField = str(arcpy.GetParameterAsText(2))
-MMSI = str(arcpy.GetParameterAsText(3))
-LengthField = str(arcpy.GetParameterAsText(4))
-VesselTypes = str(arcpy.GetParameterAsText(5))
-Simplified = str(arcpy.GetParameterAsText(6))
-CustomName = str(arcpy.GetParameterAsText(7))
-if arcpy.GetParameterAsText(8):
-    CustomTypes = [int(num) for num in arcpy.GetParameterAsText(8).split(";")]
+MultipleInt = str(arcpy.GetParameterAsText(2))
+NameField = str(arcpy.GetParameterAsText(3))
+MMSI = str(arcpy.GetParameterAsText(4))
+LengthField = str(arcpy.GetParameterAsText(5))
+VesselTypes = str(arcpy.GetParameterAsText(6))
+Simplified = str(arcpy.GetParameterAsText(7))
+CustomName = str(arcpy.GetParameterAsText(8))
+if arcpy.GetParameterAsText(9):
+    CustomTypes = [int(num) for num in arcpy.GetParameterAsText(9).split(";")]
 else:
-    CustomTypes = arcpy.GetParameterAsText(8)
-DateField = str(arcpy.GetParameterAsText(9))
-StartInterval = str(arcpy.GetParameterAsText(10))
-EndInterval = str(arcpy.GetParameterAsText(11))
-OutputName = str(arcpy.GetParameterAsText(12))
+    CustomTypes = arcpy.GetParameterAsText(9)
+DateField = str(arcpy.GetParameterAsText(10))
+StartInterval = str(arcpy.GetParameterAsText(11))
+EndInterval = str(arcpy.GetParameterAsText(12))
+OutputName = str(arcpy.GetParameterAsText(13))
 
 ## Spatial Join tool execution
-def PassageLineSummary(TrackLines, PassageLines, NameField, VesselTypes, Simplified, CustomName, CustomTypes, DateField, StartInterval, EndInterval, OutputName):
+def PassageLineSummary(TrackLines, PassageLines, MultipleInt, NameField, VesselTypes, Simplified, CustomName, CustomTypes, DateField, StartInterval, EndInterval, OutputName):
     # define temporary output for spatial join
     temp_out = "temp_spatial_join"
-    # execute spatial join
-    arcpy.AddMessage("Running spatial join...")
-    arcpy.analysis.SpatialJoin(target_features=TrackLines, join_features=PassageLines, out_feature_class=temp_out, join_operation="JOIN_ONE_TO_MANY", join_type="KEEP_ALL", field_mapping="", match_option="INTERSECT", search_radius="", distance_field_name="")
+    # execute join or intersect
 
-## Convert Spatial Join output to dataframe
-    final_fields = [field.name for field in arcpy.ListFields(temp_out)]
-    data = [row for row in arcpy.da.SearchCursor(temp_out, final_fields)]
-    temp_out_df = pd.DataFrame(data, columns=final_fields)
-    arcpy.AddMessage("Creating dataframe from spatial join output...")
+    ## count the number of input features
+    counterp = 0
+    countert = 0
+    for row in arcpy.SearchCursor(PassageLines, arcpy.ListFields(PassageLines)):
+        counterp += 1
+    for row in arcpy.SearchCursor(TrackLines, arcpy.ListFields(TrackLines)):
+        countert += 1
+    arcpy.AddMessage(f"Passage line input: {counterp} items...")
+    arcpy.AddMessage(f"Track line input: {countert} items...")
+    arcpy.AddMessage("Running Pairwise Intersect...")
+
+    ## Describe the shapetype of the passage line/area input
+    descp = arcpy.Describe(PassageLines)
+
+    ## convert to polyline if polygon, and get multipoint output
+    if descp.shapeType == "Polygon":
+        arcpy.management.PolygonToLine(in_features=PassageLines, out_feature_class="Temp_PassageLines", neighbor_option="IGNORE_NEIGHBORS")
+        arcpy.analysis.PairwiseIntersect(in_features=[TrackLines, "Temp_PassageLines"], out_feature_class=temp_out, join_attributes="ALL", cluster_tolerance="", output_type="POINT")
+    elif descp.shapeType == "Line" or descp.shapeType == "Polyline":
+        arcpy.analysis.PairwiseIntersect(in_features=[TrackLines, PassageLines], out_feature_class=temp_out, join_attributes="ALL", cluster_tolerance="", output_type="POINT")
+    ## multipart to single part, convert pairwise intersect output to individual points
+    arcpy.management.MultipartToSinglepart(in_features=temp_out, out_feature_class="temp_int")
+
+    ## count the number of multipoint and singlepoint features
+    countero = 0
+    for row in arcpy.SearchCursor("temp_int", arcpy.ListFields("temp_int")):
+        countero += 1
+    counteri = 0
+    for row in arcpy.SearchCursor(temp_out, arcpy.ListFields(temp_out)):
+        counteri += 1
+
+    arcpy.AddMessage(f"Pairwise Intersect multipart feature class output: {counteri} items...")
+    arcpy.AddMessage(f"Pairwise Intersect converted to singlepart feature class output: {countero} items...")
+
+## Convert singlepoint output to dataframe
+    final_fields = [field.name for field in arcpy.ListFields("temp_int")]
+    data = [row for row in arcpy.da.SearchCursor("temp_int", final_fields)]
+    temp_int_df = pd.DataFrame(data, columns=final_fields)
+    temp_orig_fid = temp_int_df.groupby(by="ORIG_FID").size()
+## Convert multipoint output to dataframe
+    final_fields1 = [field.name for field in arcpy.ListFields(temp_out)]
+    data1 = [row for row in arcpy.da.SearchCursor(temp_out, final_fields1)]
+    temp_out_df = pd.DataFrame(data1, columns=final_fields1)
+    temp_out_df["ORIG_FID_CALC"] = 0
+
+## Determine the number of interactions per multipoint feature. If a polygon, the total interactions per feature will be half
+## rounded to next highest integer. (if a vessel passes through an area, it will create two interactions that represent one transit.)
+    for i in temp_out_df.index:
+        if MultipleInt == "true":
+            if descp.shapeType == "Polygon":
+                temp_out_df.loc[i, "ORIG_FID_CALC"] = math.ceil(temp_orig_fid[temp_out_df["OBJECTID"][i]] / 2)
+            else:
+                temp_out_df.loc[i, "ORIG_FID_CALC"] = temp_orig_fid[temp_out_df["OBJECTID"][i]]
+        else:
+            temp_out_df.loc[i, "ORIG_FID_CALC"] = 1
+
+    arcpy.AddMessage(f"Found {temp_out_df['ORIG_FID_CALC'].sum()} interactions...")
 
 ## If a date/interval was given, select only those entries. Else, select the whole dataset.
     if pd.isnull(StartInterval) == True:
@@ -45,15 +96,12 @@ def PassageLineSummary(TrackLines, PassageLines, NameField, VesselTypes, Simplif
     if DateField != "" and StartInterval == "" and EndInterval == "":
         arcpy.AddMessage("No valid interval input, calculating for the entire dataset...")
     if DateField != "" and StartInterval != "" and EndInterval != "":
-        temp_out_df = temp_out_df[(temp_out_df["Join_Count"] > 0) & (temp_out_df[DateField] >= pd.to_datetime(StartInterval)) & (temp_out_df[DateField] <= pd.to_datetime(EndInterval))]
+        temp_out_df = [(temp_out_df[DateField] >= pd.to_datetime(StartInterval)) & (temp_out_df[DateField] <= pd.to_datetime(EndInterval))]
         arcpy.AddMessage(f"Running summary for data within the interval {StartInterval} to {EndInterval}...")
-    else:
-        temp_out_df = temp_out_df[(temp_out_df["Join_Count"] > 0)]
 
 ## find transit and unique counts
-    transit_df = temp_out_df.groupby(by=[NameField, VesselTypes]).count().reset_index()
+    transit_df = temp_out_df[[NameField, VesselTypes, "ORIG_FID_CALC"]].groupby(by=[NameField, VesselTypes]).sum().reset_index()
     unique_df = temp_out_df.groupby(by=[NameField, VesselTypes, MMSI]).count().reset_index()
-
 ## create new dataframe to summarize each vessel type for each waterway/passageline
     new_df = pd.DataFrame(columns=["Name", "Vessel_Type", "Total_Transits", "Unique_Vessels"])
     temp_type = pd.unique(temp_out_df[VesselTypes])
@@ -63,13 +111,14 @@ def PassageLineSummary(TrackLines, PassageLines, NameField, VesselTypes, Simplif
     for x in range(len(temp_label)):
         for i in range(len(temp_type)):
             new_df = new_df.append({"Name": temp_label[x], "Vessel_Type": temp_type[i]}, ignore_index=True)
+
     arcpy.AddMessage("Creating summary dataframe...")
 
 ## Populate the transit and unique counts in the new dataframe
     for y in new_df.index:
         temp_transit = transit_df[(transit_df[VesselTypes] == new_df["Vessel_Type"][y]) & (transit_df[NameField] == new_df["Name"][y])].reset_index(drop=True)
         if len(temp_transit) > 0:
-            new_df.loc[y, "Total_Transits"] = temp_transit[MMSI][0]
+            new_df.loc[y, "Total_Transits"] = temp_transit["ORIG_FID_CALC"][0]
         else:
             new_df.loc[y, "Total_Transits"] = 0
         temp_unique = unique_df[(unique_df[VesselTypes] == new_df["Vessel_Type"][y]) & (unique_df[NameField] == new_df["Name"][y])].reset_index(drop=True)
@@ -77,14 +126,18 @@ def PassageLineSummary(TrackLines, PassageLines, NameField, VesselTypes, Simplif
             new_df.loc[y, "Unique_Vessels"] = len(temp_unique[MMSI])
         else:
             new_df.loc[y, "Unique_Vessels"] = 0
+
     arcpy.AddMessage("Populating summary dataframe...")
 
 ## get list of waterway/passageline names for final summary dataframe
     Aggregate_temp = new_df.groupby(by="Name").sum().reset_index()
     Aggregate_temp = Aggregate_temp["Name"]
+
 ## check if a simplified table was selected
     if Simplified == "true":
+
         arcpy.AddMessage("Simplified dataframe selected...")
+
     ## Create simplified final summary dataframe
         if CustomName and CustomTypes:
             Passage_line_df = pd.DataFrame(
@@ -129,14 +182,12 @@ def PassageLineSummary(TrackLines, PassageLines, NameField, VesselTypes, Simplif
             Passage_line_df[f"Unique_{CustomName}"] = 0
             Passage_line_df[f"Transits_{CustomName}"] = 0
 
-
         arcpy.AddMessage("Creating aggregated summary dataframe...")
 
     ## populate the final dataframe
         for i in Passage_line_df.index:
             if LengthField:
-                maxim = [x for x in temp_out_df[temp_out_df[NameField] == Passage_line_df["Name"][i]]["Length"] if
-                         math.isnan(x) == False]
+                maxim = [x for x in temp_out_df[temp_out_df[NameField] == Passage_line_df["Name"][i]]["Length"] if math.isnan(x) == False]
             else: maxim = []
             if len(maxim) > 0:
                 Passage_line_df.loc[i, "Max_Len"] = max(maxim)
@@ -230,6 +281,7 @@ def PassageLineSummary(TrackLines, PassageLines, NameField, VesselTypes, Simplif
     arcpy.management.CreateTable(arcpy.env.workspace, OutputName)
     fields = [field for field in Passage_line_df.columns]
     datatype = []
+
     arcpy.AddMessage("Creating output table...")
 
     for i in range(len(fields)):
@@ -254,9 +306,10 @@ def PassageLineSummary(TrackLines, PassageLines, NameField, VesselTypes, Simplif
 
     ## delete cursor and temp_out spatial join
     arcpy.Delete_management(temp_out)
+    arcpy.Delete_management("Temp_PassageLines")
+    arcpy.Delete_management("temp_int")
+
     del cursor,temp_out
-
-
     return
 
 ## executable check
@@ -264,19 +317,20 @@ def PassageLineSummary(TrackLines, PassageLines, NameField, VesselTypes, Simplif
 if __name__ == '__main__':
     TrackLines = str(arcpy.GetParameterAsText(0))
     PassageLines = str(arcpy.GetParameterAsText(1))
-    NameField = str(arcpy.GetParameterAsText(2))
-    MMSI = str(arcpy.GetParameterAsText(3))
-    LengthField = str(arcpy.GetParameterAsText(4))
-    VesselTypes = str(arcpy.GetParameterAsText(5))
-    Simplified = str(arcpy.GetParameterAsText(6))
-    CustomName = str(arcpy.GetParameterAsText(7))
-    if arcpy.GetParameterAsText(8):
-        CustomTypes = [int(num) for num in arcpy.GetParameterAsText(8).split(";")]
+    MultipleInt = str(arcpy.GetParameterAsText(2))
+    NameField = str(arcpy.GetParameterAsText(3))
+    MMSI = str(arcpy.GetParameterAsText(4))
+    LengthField = str(arcpy.GetParameterAsText(5))
+    VesselTypes = str(arcpy.GetParameterAsText(6))
+    Simplified = str(arcpy.GetParameterAsText(7))
+    CustomName = str(arcpy.GetParameterAsText(8))
+    if arcpy.GetParameterAsText(9):
+        CustomTypes = [int(num) for num in arcpy.GetParameterAsText(9).split(";")]
     else:
-        CustomTypes = arcpy.GetParameterAsText(8)
-    DateField = str(arcpy.GetParameterAsText(9))
-    StartInterval = str(arcpy.GetParameterAsText(10))
-    EndInterval = str(arcpy.GetParameterAsText(11))
-    OutputName = str(arcpy.GetParameterAsText(12))
+        CustomTypes = arcpy.GetParameterAsText(9)
+    DateField = str(arcpy.GetParameterAsText(10))
+    StartInterval = str(arcpy.GetParameterAsText(11))
+    EndInterval = str(arcpy.GetParameterAsText(12))
+    OutputName = str(arcpy.GetParameterAsText(13))
 
-    PassageLineSummary(TrackLines, PassageLines, NameField, VesselTypes, Simplified, CustomName, CustomTypes, DateField, StartInterval, EndInterval, OutputName)
+    PassageLineSummary(TrackLines, PassageLines, MultipleInt, NameField, VesselTypes, Simplified, CustomName, CustomTypes, DateField, StartInterval, EndInterval, OutputName)
